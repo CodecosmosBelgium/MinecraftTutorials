1f4a9268eabf38b318e2eb1207acf3466346ef38
MaximVansteenkiste <vsmaxim@me.com>
2022-10-17 09:04:29 +0200

    Missie 1

---
+++ Reeks 1/Missie 1/main.ts
@@ -0,0 +1,17 @@
+//% color=190 weight=100 icon="\uf1ec" block="FTRPRF"
+namespace FTRPRF {
+    //% block
+    export function bouwTop() {
+        
+    }
+    //% block
+    export function bouwMidden() {
+
+    }
+    //% block
+    export function bouwBodem() {
+
+    }
+}
+
+
